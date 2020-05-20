ed99c64d52ac84af56337a2cc4336c18d0a853a7
AmineMassaabi <48286975+AmineMassaabi@users.noreply.github.com>
2020-05-20 02:44:24 +0200

    Update _settings_base.py

--- _settings_base.py
+++ _settings_base.py
@@ -3,8 +3,8 @@
 # You can get an API key from https://www.bitmex.com/app/apiKeys
 #
 
-API_KEY = 'LDo9rR_mhDhhS1oFsoD_e5FX'
-API_SECRET = '7C9EaDxEnViReBypwMctKuObEP4dQqiAW93jVoAS1-LTHGOx'
+API_KEY = ''
+API_SECRET = ''
 
 API_BASE = 'https://www.bitmex.com/api/v1/'
 # Uncomment to work on Testnet
